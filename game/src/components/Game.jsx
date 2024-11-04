e082552dd1f931d7997b15b9c6e1151480c72604
IvanSlivenko <ivan8822@ukr.net>
2024-11-04 21:07:38 +0300

    04-11-2024-21-07

--- game/src/components/Game.jsx
+++ game/src/components/Game.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
+import { calculateWinner } from '../helper';
 
 import './Game.css'
 
 
 const Game = () => {
+    const [board, setBoard]=useState(Array(9).fill(null))
+    const [xIsNext, setXIsNext] = useState(true)
+    const winner = calculateWinner(board)
+
+    const handleclick = (index) => {
+        const  boardCopy = [...board]
+  
+        if(winner || boardCopy[index]) return
+        boardCopy[index] = xIsNext ? 'X' : '0'
+        setBoard(boardCopy);
+        setXIsNext(!xIsNext)
+    }
+
+    const startNewGame = () => {
+        return (
+            <button 
+                className='start__btn' 
+                onClick={() => {
+                    setBoard(Array(9).fill(null))
+                }} 
+            >
+                Очистити поле
+            </button>
+        )
+    }
+
     return (
         <div className='wrapper'>
-           <Board /> 
+            {  startNewGame() }
+           <Board squares={board} click={handleclick}/> 
+           <p className='game__info'>
+            { winner ? 'Переміг : ' + winner : 'Ваш хід : ' + (xIsNext ? 'X' : 'O' ) }
+           </p>
         </div>
     );
 }
